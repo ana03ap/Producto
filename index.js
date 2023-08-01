4fc5c84bc6ec59bc47cc908593e236f2f9445a58
ana03ap <anxrdilx03@gmail.com>
2023-07-31 20:58:05 -0500

    new window

--- index.js
+++ index.js
@@ -5,39 +5,55 @@ let arco = document.getElementById("arco");
 let sofa = document.getElementById("imgSofa");
 let boton_colores = document.getElementsByClassName("color-fill");// cada boton de color 
 let colorText = document.getElementById("color-text");// color del titulo del lado izquierdo
+let colorBorder =  document.getElementsByClassName("color-border");
+// let ij = document.getElementById("ij");// color del titulo del lado izquierdo
+
+// const he = (x)=>{
+//     console.log("hello " + x);
+// }
 
 
-boton_colores[0].onclick=()=>{  // primer boton
-    title.innerText = "Organge Furniture";
-    arco.style.border = "48px solid #8E6841"
-    sofa.src = "img/sofa-naranja.png";
-    colorText.style.color="#8E6841";
-    boton1.style.backgroundColor="#8E6841";
+//RESETEAR TODOS BORDES
 
-    // arco.style.background="black";
-    // boton.style.background="blue";
-    // nose.style.width=`${randomNumber()}vh`
+function border (){
+
+    colorBorder[0].style.border = "1px solid transparent";
+    colorBorder[1].style.border = "1px solid transparent";
+    colorBorder[2].style.border = "1px solid transparent";
+    colorBorder[3].style.border = "1px solid transparent";
+    
+
 }
 
-boton_colores[1].onclick=()=>{  // segundo boton
-    title.innerText = "Green Furniture";
-    arco.style.border = "48px solid #585934"
-    sofa.src = "img/sofa.png";
-    colorText.style.color="#585934";
+//crear la funcion para cambiar los colores y pasarlos parametros 
+function btnColores (color,hexColor,src, numArray){
+    border ();
+    title.innerText =  `${color} Furniture`;
+    arco.style.border = `48px solid ${hexColor} `;
+    sofa.src = src;
+    colorText.style.color =hexColor;
+    colorBorder[numArray].style.border = "1px solid black";  
+    
 }
 
-boton_colores[2].onclick=()=>{  // tercer boton
-    title.innerText = "Purple Furniture";
-    arco.style.border = "48px solid #50418E"
-    sofa.src = "img/sofa-piel.png";
-    colorText.style.color="#50418E";
-}
+// llamar las funciones para cada uno de los botones 
+boton_colores[0].onclick= () => btnColores("Orange", "#8E6841", "img/sofa-naranja.png", 0); // primer boton
+boton_colores[1].onclick= () => btnColores("Green", "#585934", "img/sofa.png", 1); // segundo boton
+boton_colores[2].onclick= () => btnColores("Purple", "50418E", "img/sofa-piel.png", 2); // tercer boton
+boton_colores[3].onclick= () => btnColores("Gray", "#000000", "img/sofa-negro.png", 3); // cuatro boton
 
 
-boton_colores[3].onclick=()=>{  // cuarto boton
-    title.innerText = "Gray Furniture";
-    arco.style.border = "48px solid #000000"
-    sofa.src = "img/sofa-negro.png";
-    colorText.style.color="#000000";
-}
+
+
+// boton_colores[0].onclick=()=>{  // primer boton
+//     title.innerText = "Orange Furniture";
+//     arco.style.border = "48px solid #8E6841"
+//     sofa.src = "img/sofa-naranja.png";
+//     colorText.style.color="#8E6841";
+//     boton1.style.backgroundColor="#8E6841";
+//     colorBorder[0].style.border = "1px solid black"
+//     // arco.style.background="black";
+//     // boton.style.background="blue";
+//     // nose.style.width=`${randomNumber()}vh`
+// }
 
